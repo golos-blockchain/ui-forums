5c26a5cb8302be02454a20235527f32ad6d2a393
isteric-senior <halfyearforwhat@gmail.com>
2021-01-24 00:19:23 +0300

    Fix MDE focus problem

--- src/components/elements/post/form/MarkdownEditor/MarkdownEditor.js
+++ src/components/elements/post/form/MarkdownEditor/MarkdownEditor.js
@@ -153,9 +153,11 @@ export default class MarkdownEditor extends PureComponent {
     }
 
     focus() {
-        this._cm.focus();
-        this._cm.setCursor({ line: 999, ch: 999 });
-        document.getElementsByTagName('textarea')[1].focus();
+        setTimeout(() => {
+            this._cm.focus();
+            this._cm.setCursor({ line: 999, ch: 999 });
+            document.getElementsByTagName('textarea')[1].focus();
+        }, 100);
     }
 
     getValue() {
