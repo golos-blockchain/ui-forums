fc3ce920f432336729871f5696b0f6b695eb4951
1aerostorm <aerostorm1@gmail.com>
2021-01-30 13:11:09 +0300

    Small fixes SSR

--- webpack.server.js
+++ webpack.server.js
@@ -2,27 +2,27 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  entry: './renderserver/index.js',
+    entry: './renderserver/index.js',
 
-  target: 'node',
+    target: 'node',
 
-  externals: [nodeExternals()],
+    externals: [nodeExternals()],
 
-  output: {
-    path: path.resolve('server-build'),
-    filename: 'index.js'
-  },
+    output: {
+        path: path.resolve('server-build'),
+        filename: 'index.js'
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: 'babel-loader'
-      },
-      { test: /\.css$/, use: [ 'css-loader' ] },
-      {
-      test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
-      loader: 'url-loader?limit=100000' }
-    ]
-  }
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader'
+            },
+            { test: /\.css$/, use: [ 'css-loader' ] },
+            {
+            test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+            loader: 'url-loader?limit=100000' }
+        ]
+    }
 };
