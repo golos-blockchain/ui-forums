8ace8318a4d7e91d36c9334f632b4899ff0055c3
1aerostorm <aerostorm1@gmail.com>
2021-02-12 00:45:43 +0300

    Experiment

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,12 @@
-const { removeModuleScopePlugin } = require('customize-cra');
 
-module.exports = removeModuleScopePlugin();
+const rewireInlinSource = require('react-app-rewire-inline-source-plugin');
+const {
+	override,
+	removeModuleScopePlugin
+} = require('customize-cra');
+
+module.exports = function override (config, env) {
+    config = rewireInlinSource(config);
+    removeModuleScopePlugin()(config);
+    return config;
+}
