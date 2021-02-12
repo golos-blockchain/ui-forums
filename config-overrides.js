bbefa776414c27dcd1f9636d5e51eef138c8e5dd
1aerostorm <aerostorm1@gmail.com>
2021-02-12 16:43:34 +0300

    Embed CSS in HTML (for Google PageInsights)

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,30 @@
-const { removeModuleScopePlugin } = require('customize-cra');
+const {
+    override,
+    removeModuleScopePlugin
+} = require('customize-cra');
 
-module.exports = removeModuleScopePlugin();
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+
+function rewireInlinSourcePlugin(config) {
+    let htmlWebpackPluginIndex = null;
+    config.plugins.map((plugin, index) => {
+        if (plugin.constructor.name === 'HtmlWebpackPlugin') {
+            htmlWebpackPluginIndex = index;
+        }
+    });
+
+    if (htmlWebpackPluginIndex !== null) {
+        const htmlWebpackPlugin = config.plugins[htmlWebpackPluginIndex].constructor;
+        config.plugins[htmlWebpackPluginIndex].options.inlineSource = '.(css)$';
+        config.plugins.splice(htmlWebpackPluginIndex, 0, new HtmlWebpackInlineSourcePlugin(htmlWebpackPlugin));
+    }
+
+    return config;
+}
+
+
+module.exports = function override(config, env) {
+    config = rewireInlinSourcePlugin(config);
+    removeModuleScopePlugin()(config);
+    return config;
+}
