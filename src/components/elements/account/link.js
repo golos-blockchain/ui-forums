01a373bd0bb7a11c62f1a019efbafe5fe6a0b4c0
1aerostorm <aerostorm1@gmail.com>
2021-04-02 14:00:56 +0300

    Private messages - write button

--- src/components/elements/account/link.js
+++ src/components/elements/account/link.js
@@ -2,19 +2,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import tt from 'counterpart';
 
-import { Popup } from 'semantic-ui-react';
+import { Popup, Icon } from 'semantic-ui-react';
 
 import AccountCard from '../../../containers/account/card';
 
 export default class AccountLink extends React.Component {
     render() {
-        const { color, username, reputation, noPopup, isBanned } = this.props;
+        const { color, username, reputation, withMsgsButton, noPopup, isBanned } = this.props;
         const content = this.props.content || `@${username}`;
-        const link = (
+        let link = (
             <Link to={`/@${username}`} style={{color, textDecoration: isBanned ? 'line-through' : undefined}}>
                 {content}
             </Link>
         );
+        if (withMsgsButton) {
+            link = (
+                <span>
+                    {link}
+                    &nbsp;
+                    <Link to={`/msgs/@${username}`} target='_blank'>
+                        <Icon name='envelope outline' color='blue' title={tt('g.write')} />
+                    </Link>
+                </span>
+            );
+        }
         if (noPopup) return link;
         return (<span>
                 <AccountCard
@@ -27,6 +38,14 @@ export default class AccountLink extends React.Component {
                     mouseEnterDelay={500}
                     inverted
                 /> : null}
+                {withMsgsButton ? (
+                    <span>
+                        &nbsp;
+                        <Link to={`/msgs/@${username}`} target='_blank'>
+                            <Icon name='envelope outline' color='blue' title={tt('g.write')} />
+                        </Link>
+                    </span>
+                ) : null}
             </span>
         );
     }
