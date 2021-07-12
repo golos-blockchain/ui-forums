9ff7324fd3d8bf78490d54742aac014e842459c0
1aerostorm <aerostorm1@gmail.com>
2021-07-12 18:16:04 +0300

    Private messages - remove tarantool, use golos notify

---
+++ src/utils/notifications.js
@@ -0,0 +1,23 @@
+import { Signature, hash } from 'golos-classic-js/lib/auth/ecc';
+
+import { notifyApiLogin, notifyApiLogout } from './NotifyApiClient';
+
+export async function notifyLogin(account, posting_key) {
+    const res = await notifyApiLogin(account, null);
+    console.log('login_challenge', res.login_challenge);
+
+    const signatures = {};
+    const challenge = {token: res.login_challenge};
+    const bufSha = hash.sha256(JSON.stringify(challenge, null, 0));
+    const sign = (role, d) => {
+    if (!d) return;
+        const sig = Signature.signBufferSha256(bufSha, d);
+        signatures[role] = sig.toHex();
+    };
+    sign('posting', posting_key);
+    await notifyApiLogin(account, signatures);
+}
+
+export async function notifyLogout() {
+    await notifyApiLogout();
+}
